476761c267a2aae014032a72f86148605f3464b7
Philipp Duffner <philipp@phphaus.com>
2014-10-20 11:31:05 +1000

    Just install package for Ubuntu

--- recipes/default.rb
+++ recipes/default.rb
@@ -23,21 +23,29 @@ freetds_url = node['freetds']['url'] ||
   "http://mirrors.ibiblio.org/freetds/stable/freetds-#{version}.tar.gz"
 configure_options = "--with-tdsver=#{node['freetds']['tds_version']} #{'--disable-odbc' unless node['freetds']['odbc']}"
 
-remote_file "#{Chef::Config[:file_cache_path]}/freetds-#{version}.tar.gz" do
-  action :create_if_missing
-  backup false
-  source freetds_url
-  checksum node['freetds']['checksum'] if node['freetds']['checksum']
-end
+if platform?("ubuntu")
+  # just install as package
+  apt_package "freetds-bin freetds-common freetds-dev libct4 libsybdb5" do
+    action :install
+  end
+else
+  # get and build from sources
+  remote_file "#{Chef::Config[:file_cache_path]}/freetds-#{version}.tar.gz" do
+    action :create_if_missing
+    backup false
+    source freetds_url
+    checksum node['freetds']['checksum'] if node['freetds']['checksum']
+  end
 
-bash 'build freetds' do
-  cwd Chef::Config[:file_cache_path]
-  code <<-EOF
+  bash 'build freetds' do
+    cwd Chef::Config[:file_cache_path]
+    code <<-EOF
     tar zxf freetds-#{version}.tar.gz
     (cd freetds-#{version} && CFLAGS='-fPIC' ./configure #{configure_options})
     (cd freetds-#{version} && make && make install)
-  EOF
-  not_if 'which tsql'
+    EOF
+    not_if 'which tsql'
+  end
 end
 
 template '/usr/local/etc/freetds.conf' do
